e03f4d35cca688d2de36f37c582e5fb49a892b40
rishaabhgautham-del <rishaabh.gautham@gmail.com>
2026-02-01 21:16:56 -0600

    Update pyproject.py

--- pyproject.py
+++ pyproject.py
@@ -1,44 +1,23 @@
-import numpy as np
+#number, name, salary, department, find the sum of employee salary based on department
 import pandas as pd
 
+data_emp = pd.DataFrame(
+    [[120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130], 
+     ["Tim", "Bob", "Harry", "Sam", "Mike", "Kit", "Blake", "Art", "Mark", "Lee", "John"],
+     [62300, 145900, 201800, 78400, 389600, 456200, 273100, 118500, 334700, 492000, 500000], 
+     ["Marketing", "Production", "Finance", "HR", "Finance", "Finance", "Marketing", "Production", "HR", "HR", "Production"]], 
+    columns = ["Number", "Name", "Salary", "Department"], index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 
-# 1. Create the DataFrame
-data1 = pd.DataFrame([
-    [420, 380, 390], 
-    [900, 1000, 3000]
-], columns = ["First Class", "Second Class", "Third Class"], 
-   index = ["Survived", "Died"])
+#can use groupby and agg of sum to find
+data_emp = data_emp.T
+grouped = data_emp.groupby("Department").agg('sum')
 
-# 1: Inspection ---
-# View basic info about the dataset
-print("--- Data Info ---")
-print(data1.info()) 
 
-# 2: Describing the Data ---
-# Use describe() to get a statistical summary of the columns
-print("\n--- Statistical Summary ---")
-print(data1.describe())
 
-# 3: Aggregation ---
-# Calculate the total passengers in each class (Column Sum)
-class_totals = data1.sum()
-print("\n--- Total Passengers per Class ---")
-print(class_totals)
+#1. Load dataset from csv file (without using pandas, and with using pandas, use pandas from excel file, w/o header name, pipe seperated, and json file)
 
-# Calculate the total outcomes (Row Sum)
-outcome_totals = data1.sum(axis=1)
-print("\n--- Total Survived vs Died ---")
-print(outcome_totals)
+#2. Display records from department A alone
 
-# 4: Finding Max/Min ---
-# Identify which class had the highest number of deaths
-# Note: Since 'Died' is an index, we look at the 'Died' row
-print("\n--- Deaths per Class ---")
-print(data1.loc["Died"])
-print(f"\nMax Deaths: {data1.loc['Died'].max()}")
+#3. Summary statistics of people who are getting more than $5,000 in salary (Who are working in city of SA)
 
-# 5: Calculating Percentages ---
-# Calculate the percentage of survival for First Class
-first_class_total = data1["First Class"].sum()
-first_class_survival = (data1.loc["Survived", "First Class"] / first_class_total) * 100
-print(f"\nFirst Class Survival Rate: {first_class_survival:.2f}%")
+#4. 
